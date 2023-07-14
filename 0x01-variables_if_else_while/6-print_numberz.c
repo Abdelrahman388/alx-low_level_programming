7bfc5fa4d8462c277cab433d2cf9b55506ca9b11
Abdelrahman388 <abdelrahmansameh092@gmail.com>
2023-07-14 16:34:06 -0700

    5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * Description: print all single digit numbers using putchar wit int variable
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar("\n");
+
+	return (0);
+}
