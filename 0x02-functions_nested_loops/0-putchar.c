5c3d77e726312e8fb9bc0f6584995b6485969ebf
Abdelrahman388 <abdelrahmansameh092@gmail.com>
2023-07-18 16:30:06 -0700

    0task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *  main - entery point
+ *
+ *  Description: prints _putchar using putchar prototype
+ *
+ *  Return: always 0 (success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchr('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_K
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+#endif
