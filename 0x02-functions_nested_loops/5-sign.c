98543069af30e15d442c9956132c487c52ab13cb
Abdelrahman388 <abdelrahmansameh092@gmail.com>
2023-07-18 19:13:03 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - print + if integer is positive & - if it is negative and 0 if it is zero
+ *
+ * @c: takes integer tyoe input for function
+ *
+ * Return: 1 if +, 0 if 0and -1 if -
+*/
+
+int print_sign(int n)
+{
+	if (n >	0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
