f6955c7d233ee6c6bd04c7d9c855e2bc15f7ae9f
Abdelrahman388 <abdelrahmansameh092@gmail.com>
2023-07-19 19:48:22 -0700

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 
 /**
  * main - entery point
- * 
+ *
  * Description: computes the sum of all multiples of 3 or 5
  *
  * Return: always 0
@@ -11,8 +11,8 @@
 int main(void)
 {
 	int s, n;
-	
-	for(n = 0; n < 1024; n++)
+
+	for (n = 0; n < 1024; n++)
 	{
 		if ((n % 3 == 0) || (n % 5 == 0))
 			s += n;
