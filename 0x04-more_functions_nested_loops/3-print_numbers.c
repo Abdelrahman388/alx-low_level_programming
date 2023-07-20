a6ecbd954c9fcdf8d1e3905a74ad316808988c88
Abdelrahman388 <abdelrahmansameh092@gmail.com>
2023-07-20 14:27:36 -0700

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,18 @@
 
 /**
  * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: 0
 */
 
 void print_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar("%c", i + 48);
-	}
+	do {
+		_putchar(i + 48)
+			i++;
+	} while (i >= 0 && i <= 9);
 	_putchar("\n");
 }
 
