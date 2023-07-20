6653fa45facdb3bf5104ba4771edeff2116dd811
Abdelrahman388 <abdelrahmansameh092@gmail.com>
2023-07-20 15:55:13 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - 0rint a straight line
+ *
+ * @n: the number of times
+*/
+
+void print_line(int n)
+{
+	int lnch;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (*lnch = 1; lnch <= n; lnch++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
