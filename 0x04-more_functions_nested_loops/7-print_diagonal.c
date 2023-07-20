9f8cd21c80db2fe7126aa8c46c750e6ccebce89d
Abdelrahman388 <abdelrahmansameh092@gmail.com>
2023-07-20 16:14:01 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_line - 0rint a straight line
+ * print_diagonal - print a straight line
  *
  * @n: the number of times
 */
 
-void print_line(int n)
+void print_diagonal(int n)
 {
 	int p, s;
 
