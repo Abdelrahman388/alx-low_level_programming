5bd18825eed0d776f0245cf80691f63f94168764
Abdelrahman388 <abdelrahmansameh092@gmail.com>
2023-07-20 16:59:59 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - entery point
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && !(n % 5 ==0))
+			printf("Fizz");
+		else if (n % 5 == 0 && !(n % 3 ==0))
+			printf("Buzz");
+		else if (n % 5 == 0 && n % 3 ==0)
+			printf("FizzBuzz");
+		else
+			printf("%d", n);
+
+		if (n != 100)
+			printf(" ");
+		else
+			ptintf("\n");
+	}
+
+	return (0);
+}
