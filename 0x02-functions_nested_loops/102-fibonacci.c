96946ac08fde1d2c826f50ccfd075875e52f0a91
Abdelrahman388 <abdelrahmansameh092@gmail.com>
2023-07-19 20:02:16 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - entery point
+ *
+ * Description: prints the first 50 fibonacci numbers starting with 1 and 2
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int c;
+	unsigned long fib1 = 0, fib2 = 1, s;
+
+	for (c = 0; c < 50; c++)
+	{
+		s = fib1 + fib2;
+		printf("%lu", s);
+		
+		fib1 = fib2;
+		fib2 = s;
+
+		if (c == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
