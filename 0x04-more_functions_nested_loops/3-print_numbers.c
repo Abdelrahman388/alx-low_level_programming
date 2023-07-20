b74ba3bfa8d41ecbab2da4588c2f8d6d46f76977
Abdelrahman388 <abdelrahmansameh092@gmail.com>
2023-07-20 14:18:54 -0700

    task4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers from 0 to 9
+ * print_numbers - prints numbers from 0 to 9
 */
 
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	for ( int i = 0; i <= 9; i++)
 	{
-		if ((i != 2) || (i != 4))
-			_putchar("%d", i + 48);
+		_putchar("%d", i + 48);
 	}
 	_putchar("\n");
 }
